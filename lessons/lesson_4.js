8117d3cf501f8331228ce331dfe78389df378dc0
chsergey80 <chsergey@yandex.ru>
2023-08-18 02:04:08 +0300

    current_commit

--- lessons/lesson_4.js
+++ lessons/lesson_4.js
@@ -5,5 +5,11 @@ const myCity = {
 }
 myCity.city = 'Las Vegas';  // меняем свойство
 myCity.country = 'USA';   // добавляем свойство
+delete myCity.country   //удаление свойства
+myCity['popular'] = true;   //Добавляем свойство
+const countryPropertyName = 'country';  // вводим переменную, которая будет новым или имеющимся свойством
+myCity[countryPropertyName] = 'Russia';  // задаем значение этому свойству
+
+
 console.log(myCity);
 
