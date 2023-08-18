2540068cc7e73e37e9f06f7f8c786a0f22a07433
chsergey80 <chsergey@yandex.ru>
2023-08-19 02:29:07 +0300

    current_commit

--- lessons/lesson_5.js
+++ lessons/lesson_5.js
@@ -1,1 +1,14 @@
 // Мутации в JS
+const person = {
+  name: 'Bob',
+  age: 25,
+}
+const person2 = person
+person2.age = 26
+person2.isAdult = true
+console.log(person.age);
+console.log(person2.isAdult);
+console.log(person);
+console.log(person2);
+
+//
