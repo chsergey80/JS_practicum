bc2cd662bebc1c5a591800cc5ec247ee13bd5bbb
chsergey80 <chsergey@yandex.ru>
2023-08-23 03:47:03 +0300

    current_commit

--- lessons/lesson_4_1_flags and property descriptors.js
+++ lessons/lesson_4_1_flags and property descriptors.js
@@ -8,7 +8,19 @@ console.log(JSON.stringify(descriptor, null, 2));
 //Чтобы изменить флаги, мы можем использовать метод Object.defineProperty
 let user1 = {};
 Object.defineProperty(user1, "name", {
-  value: "John"
+  value: "John",
+  writable: true,
 });
 let descriptor1 = Object.getOwnPropertyDescriptor(user1, 'name');
 console.log( JSON.stringify(descriptor1, null, 2 ) );
+
+//Сделаем свойство user.name доступным только для чтения. Для этого изменим флаг writable
+let user3 = {
+  name: "John"
+};
+Object.defineProperty(user3, "name", {
+  writable: false
+});
+user.name = "Pete";  // Ошибка: Невозможно изменить доступное только для чтения свойство 'name'
+
+
