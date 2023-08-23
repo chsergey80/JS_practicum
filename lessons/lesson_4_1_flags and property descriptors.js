c4fb475065869b63ff95dcf6ffdf7030b18954b3
chsergey80 <chsergey@yandex.ru>
2023-08-23 03:39:42 +0300

    current_commit

--- lessons/lesson_4_1_flags and property descriptors.js
+++ lessons/lesson_4_1_flags and property descriptors.js
@@ -5,3 +5,10 @@ let user = {
 let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
 console.log(JSON.stringify(descriptor, null, 2));
 
+//Чтобы изменить флаги, мы можем использовать метод Object.defineProperty
+let user1 = {};
+Object.defineProperty(user1, "name", {
+  value: "John"
+});
+let descriptor1 = Object.getOwnPropertyDescriptor(user1, 'name');
+console.log( JSON.stringify(descriptor1, null, 2 ) );
