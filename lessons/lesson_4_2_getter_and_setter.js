8c44c07ae2ae1127d2e1a0770eb27d16d9c55786
chserge80 <chsergey@yandex.ru>
2023-08-25 19:41:25 +0300

    current_commit

--- lessons/lesson_4_2_getter_and_setter.js
+++ lessons/lesson_4_2_getter_and_setter.js
@@ -1,1 +1,17 @@
 //Свойства - геттеры и сеттеры
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  }
+};
+
+user.fullName = "Alice Cooper"; // set fullName запустится с данным значением
+console.log(user.name); // Alice
+console.log(user.surname); // Cooper
